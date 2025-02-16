0eb5ecabd0026dd44483f471b80135cc0d43fd0e
Mina Emad <minaemad6060@gmail.com>
2025-02-16 16:39:18 +0200

    Initial Commit

---
+++ SocialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialApp
+//
+//  Created by Mina Emad on 16/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
