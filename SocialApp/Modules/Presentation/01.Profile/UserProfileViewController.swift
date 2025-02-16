03bbad9979d5062ffcf498af6bf2efdcafb528a3
Mina Emad <minaemad6060@gmail.com>
2025-02-16 21:26:22 +0200

    create and design UserProfileViewController screen

--- SocialApp/Modules/Presentation/01.Profile/UserProfileViewController.swift
+++ SocialApp/Modules/Presentation/01.Profile/UserProfileViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var albumsTableView: UITableView!
+
+    // MARK: - Properties
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+    // MARK: - LifeCycle-Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTableView()
+    }
+    
+    // MARK: - Functions
+    private func setupTableView() {
+        albumsTableView.delegate = self
+        albumsTableView.dataSource = self
+        albumsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
+        
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - Setup-TableView
+extension UserProfileViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+        
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected: \(items[indexPath.row])")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
